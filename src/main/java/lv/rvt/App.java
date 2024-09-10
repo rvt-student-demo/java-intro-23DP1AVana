90066c1008877480972fbf986312474270b9c199
23DP1AVana <166118287+23DP1AVana@users.noreply.github.com>
2024-09-10 10:37:39 +0000

    Nepilna versija

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,12 +2,30 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.Scanner;
+// uzveudihan skaneri vienu reizi
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Sveiks" + input);
+
+        String name = scanner.nextLine();
+
+        System.out.println();
+        String group = scanner.nextLine();
+        // Izvade viena rinda iznamotjot 3 mainigos
+        // Output example
+        System.out.println("sveiks"+ scanner);
+        
     }
 
 }
+
+
+
+
